476077b7d5598a3ada62581620d59d16e619265e
Aribidesi Fawaz Kayode <kayodearibidesi1@gmail.com>
2023-07-13 17:34:36 -0700

    Hello World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)", sizeof(long long int));
 	printf("Size of a flot: %d byte(s)", sizeof(float));
-	retun (0);
+	return (0);
 }

a625c2824176ff848da3dc16af1cb820a86aa27b
Aribidesi Fawaz Kayode <kayodearibidesi1@gmail.com>
2023-10-07 17:47:06 -0700

    correction

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
-/**
-  * main - print three number combination
-  *
-  * Return : 0
-  */
 int main(void)
-	int i = '0';
-	int j = '0';
-	int k = '0';
-
-	while (i <= '7')
+{
+	for (int i = 0; i < 10; i++)
+	{
+	for (int j = i + 1; j < 10; j++)
+	{
+	putchar('0' + i);
+	putchar('0' + j);
+	if (i != 8 || j != 9)
+	{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
